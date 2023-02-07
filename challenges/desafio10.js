1677620cbadbb6a89b0441528fe6b763980af7b3
Vitor Lichoti <dev.itor@pop-os.localdomain>
2023-02-07 16:19:54 -0400

    feat/ requisito 9, 10, 11, 12, 13, 14

---
+++ challenges/desafio10.js
@@ -0,0 +1,11 @@
+db.produtos.find({
+  valoresNutricionais: {
+    $elemMatch: {
+      tipo: "proteínas",
+      $and: [
+        { percentual: { $gte: 30 } },
+        { percentual: { $lte: 40 } }
+      ]
+    }
+  }
+}, { _id: 0, nome: 1 });
