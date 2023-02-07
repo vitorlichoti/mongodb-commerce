6c5042d438fd5c00fadbf0735b6c824db5a0765a
Vitor Lichoti <dev.itor@pop-os.localdomain>
2023-02-07 19:30:01 -0400

    feat/ correção de lint

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -18,4 +18,4 @@ db.produtos.updateMany({
   },
 });
 
-db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 })
+db.produtos.find({}, { _id: 0, nome: 1, vendasPorDia: 1 });
--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -6,7 +6,6 @@ db.produtos.updateMany({},
         $sort: 1,
       },
     },
-  },
-);
+  });
 
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
